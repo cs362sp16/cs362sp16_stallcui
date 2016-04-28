f8fb1526257ffeec00df07d661385f28a5133366
Istallcup <stallcui@oregonstate.edu>
2016-04-27 20:11:04 -0700

    assignmen2: modified makefile, started to develop test 1

--- dominion/unittest1.c
+++ dominion/unittest1.c
@@ -1,9 +1,15 @@
 #include "dominion.h"
 #include "tests.h"
+#include <stdio.h>
 
 int main()
 {
+
+	printf("UNIT TEST ONE -- RUNNING\n");	
+
 	struct gameState g;
 
-	int k[10] = {adventurer, council_room, feast, gardens, mine, smithy, village, baron, minion, tribute};
+	int k[10] = {adventurer, council_room,
+	  feast, gardens, mine, smithy, village, baron, minion, tribute};
+
 }
---
+++ dominion/unittest2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("UNIT TEST TWO -- RUNNING\n");
+	
+	return 0;
+}
